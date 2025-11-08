04f62707ef1c516d5cee763a3752e9c9d89f7275
Santiago Castro <zantiago2320@gmail.com>
2025-11-08 00:04:41 -0500

    funcionalides de datos entrenador

--- sabi/src/main/java/com/sabi/sabi/SabiApplication.java
+++ sabi/src/main/java/com/sabi/sabi/SabiApplication.java
@@ -2,8 +2,10 @@ package com.sabi.sabi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class SabiApplication {
 
 	public static void main(String[] args) {
